f88ad2df4e916fce9fbeb3a8f98cac21397b248d
ykl7 <yklal95@gmail.com>
2016-11-17 23:02:35 +0530

    text extracted, structure to be made now

--- extract.py
+++ extract.py
@@ -2,9 +2,17 @@ import json
 import xmltodict
 from bs4 import BeautifulSoup
 
-file_path = "" # file to be scraped
+file_path = "/Users/apple/Documents/Python/GRETIL-Prakrit/Prakrit/"
 
-f = open(file_path)
-data = f.read().decode('utf8')
-data = BeautifulSoup(data).get_text()
-print data
+# file to be scraped
+file_name = ""
+
+# output file name
+out_file_name = ""
+
+f = open(file_path+file_name)
+data = f.read().decode('utf-8')
+data = BeautifulSoup(data, "html5lib").get_text().encode('utf-8')
+
+out = open(file_path+out_file_name, "w+")
+out.write(data)
